99cf6ddf6e2fcca367076166cc638547489ae51d
Indra Fajar Nurwahid <fajarnurwahid99@gmail.com>
2024-12-18 10:02:03 +0700

    fix: Correct CORS configuration
    Improve the handling of allowed origins in CORS
    configuration to correctly process comma-separated
    values from environment variables.  This prevents
    unexpected CORS errors.

--- src/main/java/com/pos/point_of_sale/common/config/WebConfig.java
+++ src/main/java/com/pos/point_of_sale/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(System.getenv("ALLOWED_CORS").split(","))
+                .allowedOrigins(System.getenv("ALLOWED_CORS"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
