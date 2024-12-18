036f4c02725c568bb0ed9a380c5690a418c4c407
Indra Fajar Nurwahid <fajarnurwahid99@gmail.com>
2024-12-18 07:22:55 +0700

    feat: Implement Spring Security
    Integrate Spring Security for enhanced application security.
    
    Add CORS configuration for cross-origin requests.
    
    Improve security measures and handle authentication.

--- src/main/java/com/pos/point_of_sale/common/config/WebConfig.java
+++ src/main/java/com/pos/point_of_sale/common/config/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins(System.getenv("ALLOWED_CORS").split(",")) // Allowed origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
-                .allowedHeaders("Authorization", "Content-Type") // Allowed headers
-                .allowCredentials(true); // Allow cookies
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(System.getenv("ALLOWED_CORS").split(","))
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
+
